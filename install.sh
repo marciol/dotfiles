14be731d048d04846722c09c2cce23e802d03535
Marcio Faria <marcitolopes@gmail.com>
2021-12-23 16:11:23 -0300

    install brew bundle as last step

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ brew install stow git
 
 # Dotfiles
 echo "Installing the dotfiles"
-git clone git@github.com:marciol/dotfiles.git $HOME/.dotfiles
+[ ! -d $HOME/.dotfiles ] && git clone git@github.com:marciol/dotfiles.git $HOME/.dotfiles
 
 pushd $HOME/.dotfiles
 
@@ -30,43 +30,42 @@ stow brew
 
 popd
 
-# # Brewbundle
-# echo "Brew bundle ..."
-# brew bundle
-#
-# # Git
-# echo "Installing git"
-# git config --global core.excludesfile ~/.gitignore_global
-#
-#
-# # Direnv
-# echo "Installing direnv"
-# curl -sfL https://direnv.net/install.sh | bash
-#
-# # Fzf
-# echo "Installing fzf"
-# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-# ~/.fzf/install --key-bindings --completion --no-update-rc
-#
-# # Rust
-# echo "Installing rust"
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh --profile default
-#
-# # Asdf
-# echo "Installing asdf"
-# [ ! -d $HOME/.asdf ] && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
-# source $HOME/.asdf/asdf.sh
-# asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-# asdf plugin-add java https://github.com/halcyon/asdf-java.git
-# asdf plugin-add lein https://github.com/miorimmax/asdf-lein.git
-# asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-# asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-# asdf plugin-add rebar https://github.com/Stratus3D/asdf-rebar.git
-# asdf plugin-add lfe https://github.com/vic/asdf-lfe.git
-# asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-# asdf plugin-add clojerl https://github.com/clojerl/asdf-clojerl.git
-# asdf install
-#
+# Git
+echo "Installing git"
+git config --global core.excludesfile ~/.gitignore_global
+
+# Direnv
+echo "Installing direnv"
+curl -sfL https://direnv.net/install.sh | bash
+
+# Fzf
+echo "Installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --no-update-rc
+
+# Rust
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh --profile default
+
+# Asdf
+echo "Installing asdf"
+[ ! -d $HOME/.asdf ] && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+source $HOME/.asdf/asdf.sh
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-add lein https://github.com/miorimmax/asdf-lein.git
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin-add rebar https://github.com/Stratus3D/asdf-rebar.git
+asdf plugin-add lfe https://github.com/vic/asdf-lfe.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add clojerl https://github.com/clojerl/asdf-clojerl.git
+asdf install
+
+# Brewbundle
+echo "Brew bundle ..."
+brew bundle
+
 # # Clojure
 # echo "Installing clojure"
 # brew install clojure/tools/clojure
