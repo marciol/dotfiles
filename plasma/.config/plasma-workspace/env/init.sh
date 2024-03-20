1c2e3ade012a8ccc7545df41112a67e4681780b1
Márcio Faria <marcitolopes@gmail.com>
2024-03-20 18:38:36 -0300

    Update

--- plasma/.config/plasma-workspace/env/init.sh
+++ plasma/.config/plasma-workspace/env/init.sh
@@ -1,5 +1,6 @@
+# export GTK_USE_PORTAL=1
 # export GTK_IM_MODULE=cedilla
 # export QT_IM_MODULE=cedilla
+# export ELECTRON_OZONE_PLATFORM_HINT=wayland
+export XMODIFIERS=@im=fcitx
 export MOZ_ENABLE_WAYLAND=1
-export ELECTRON_OZONE_PLATFORM_HINT=wayland
-export GTK_USE_PORTAL=1
